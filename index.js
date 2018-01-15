0dca68680211818ffdd5072921c66573bc9b6c35
Michelle Ranly <meranly8@gmail.com>
2018-01-15 02:02:58 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,3 +5,7 @@ var kittens = ['Milo', 'Otis', 'Garfield']
 function destructivelyAppendKitten(name) {
   kittens.push('Ralph')
 }
+
+function destructivelyPrependKitten(name) {
+  kittens.unshift('')
+}
